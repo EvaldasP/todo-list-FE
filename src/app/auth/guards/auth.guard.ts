286fc550f5714e56a726341ad5f10bc4272abbde
Evaldas Pocius <evaldukas96@gmail.com>
2023-01-19 22:04:25 +0200

    feat(login) navigate to login page if not logged in

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private readonly _authService: AuthService,
-    public router: Router
+    private readonly _router: Router
   ) {}
   canActivate(): Observable<boolean> {
-    return this._authService.isLoggedIn$;
+    return this._authService.isLoggedIn$.pipe(
+      tap((isLoggedIn) => {
+        if (!isLoggedIn) {
+          this._router.navigate(['login']);
+        }
+      })
+    );
   }
 }

499e366a350539dfd77a5dfa50a04f6db40df7f8
Evaldas Pocius <evaldukas96@gmail.com>
2023-01-19 22:21:01 +0200

    feat(login) default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path: 'dashboard',
